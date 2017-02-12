f5a7cd11ba735bf12429bf47afb9642028678d15
philip <philip.clarke@redhood.co.uk>
2017-02-12 11:18:17 +0000

    added a simple pi call

--- public/core.js
+++ public/core.js
@@ -1,6 +1,14 @@
 var app = angular.module('myApp', ['ui.bootstrap']);
 
-app.controller('MainCtrl', function($scope){
-
+app.controller('MainCtrl', function($scope, $http){
+  $http({
+  method: 'GET',
+  url: 'https://api.github.com/users/defunkt'
+}).then(function successCallback(response) {
+    $scope.data=response.data;
+    console.log($scope.data)
+  }, function errorCallback(response) {
+    console.log(response)
+  });
 
 });

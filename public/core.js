8283311c6c5a5d54c830ef606ed9c519ec94086a
philip <philip.clarke@redhood.co.uk>
2017-02-13 10:48:09 +0000

    added an access token - now retrieving data from the api

--- public/core.js
+++ public/core.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', ['ui.bootstrap']);
 app.controller('MainCtrl', function($scope, $http){
   $http({
   method: 'GET',
-  url: 'https://api.github.com/users/defunkt'
+  url: 'https://api.github.com/orgs/squirrelscript/members?access_token=33cc696bdfa485a200552abaa2af393a164215fe'
 }).then(function successCallback(response) {
     $scope.data=response.data;
     console.log($scope.data)

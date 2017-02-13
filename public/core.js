6a2e88b2ad603a8f7819c4721a7c46d54724c563
philip <philip.clarke@redhood.co.uk>
2017-02-13 18:07:17 +0000

    add user logins as links

--- public/core.js
+++ public/core.js
@@ -3,7 +3,7 @@ var app = angular.module('myApp', ['ui.bootstrap','ui.router']);
 app.controller('MainCtrl', function($scope, $http){
   $http({
   method: 'GET',
-  url: 'https://api.github.com/orgs/squirrelscript/members?access_token=5934703bafb951289a1f5d63869ba1ced74e270b'
+  
 }).then(function successCallback(response) {
     $scope.data=response.data;
     console.log($scope.data)

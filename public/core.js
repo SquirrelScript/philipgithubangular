c9b6635383a7de8ab49fd89a3064aec7f77f9b52
philip <philip.clarke@redhood.co.uk>
2017-02-13 10:58:17 +0000

    added an access token - replaced old one

--- public/core.js
+++ public/core.js
@@ -3,12 +3,12 @@ var app = angular.module('myApp', ['ui.bootstrap']);
 app.controller('MainCtrl', function($scope, $http){
   $http({
   method: 'GET',
-  url: 'https://api.github.com/orgs/squirrelscript/members?access_token=33cc696bdfa485a200552abaa2af393a164215fe'
+  url: 'https://api.github.com/orgs/squirrelscript/members?access_token=379f35fb116fb6c584be33db6736debea3740657'
 }).then(function successCallback(response) {
     $scope.data=response.data;
     console.log($scope.data)
-  }, function errorCallback(response) {
-    console.log(response)
+  }, function errorCallback(err) {
+    console.log(err)
   });
 
 });
